5e37fcd21257dbf3bd9bb75628596b644ddeaabc
ZexuanMA <richard66zexuan@gmail.com>
2025-02-23 21:02:49 +0800

    Create Code_for_adding_Font.py

---
+++ Code_for_adding_Font.py
@@ -0,0 +1,6 @@
+import matplotlib.font_manager.fontManager
+
+#An example, if you want to get the figure's font you want. 
+
+font_path='times.ttf'
+matplotlib.font_manager.fontManager.addfont(font_path)
